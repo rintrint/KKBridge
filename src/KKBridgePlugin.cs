99a56f9641d14c41f95d1b12e67738f715bb7846
rintrint <152774318+rintrint@users.noreply.github.com>
2026-01-01 03:12:19 +0800

    v0.0.5

--- src/KKBridgePlugin.cs
+++ src/KKBridgePlugin.cs
@@ -115,7 +115,7 @@ namespace KKBridge
         }
     }
 
-    [BepInPlugin("com.rintrint.kkbridge", "KKBridge", "0.0.4")]
+    [BepInPlugin("com.rintrint.kkbridge", "KKBridge", "0.0.5")]
     public class KKBridgePlugin : BaseUnityPlugin
     {
         internal static ManualLogSource Log;
